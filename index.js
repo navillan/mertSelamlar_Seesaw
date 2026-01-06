4b8a39059e0c96a5df747ff2f37e31a0423d497f
Mert <espawnn@gmail.com>
2026-01-06 12:33:34 +0300

    design structures v0.2

---
+++ index.js
@@ -0,0 +1,7 @@
+const leftWeigth = document.getElementsByClassName("weigth-left");
+const rightWeigth = document.getElementsByClassName("weigth-right");
+const nextWeigth = document.getElementsByClassName("weigth-next");
+const plankAngleInfo = document.getElementsByClassName("plank-angle-info");
+const gameFrame = document.getElementsByClassName("game-frame");
+const plank = document.getElementsByClassName("plank");
+const resetButton = document.getElementsByClassName("button-reset")
