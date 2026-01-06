7b405cf0f807793ef4ec8651ee040b498909c082
Mert <espawnn@gmail.com>
2026-01-06 15:04:01 +0300

    Game structure v0.4

--- index.js
+++ index.js
@@ -14,6 +14,21 @@ let nextBallWeigth = Math.round(Math.random() * 9) + 1;
 
 let ball = null;
 
+ballFrame.addEventListener("pointermove", (event) => {
+  const box = ballFrame.getBoundingClientRect();
+  if(
+    event.clientX >= box.left &&
+    event.clientX <= box.right &&
+    event.clientY >= box.top &&
+    event.clientY <= box.bottom
+  ) {
+    if(ball) {
+      ball.style.left = (event.clientX - box.left) + "px";
+      ball.style.top = (event.clientY - box.top) + "px";
+    }
+  }
+});
+
 function createBall(event){
   const currentBallWeigth = nextBallWeigth;
 
@@ -25,9 +40,9 @@ function createBall(event){
   ball.style.width = (nextBallWeigth * 8) + "px";
   ball.style.height = (nextBallWeigth * 8) + "px";
   ball.style.fontSize = (currentBallWeigth * 3) + "px";
-  ballFrame.append(ball)
-  
   ball.textContent = currentBallWeigth + "kg";
+  ballFrame.append(ball);
+  
   return ball
 };
 
@@ -35,3 +50,33 @@ function deleteBall(){
   const allBalls = ballFrame.querySelectorAll(".ball");
   allBalls.forEach(ball => ball.remove());
 };
+
+function dropBall(event){
+  const box = ballFrame.getBoundingClientRect();
+  const currentBallWeigth = nextBallWeigth;
+
+  const ball = document.createElement('div');
+  ball.classList.add('ball');
+  ball.style.position = "absolute";
+  ball.style.left = (event.pageX - box.left) + "px";
+  ball.style.top = (event.pageY - box.top) + "px";
+  ball.style.width = (nextBallWeigth * 8) + "px";
+  ball.style.height = (nextBallWeigth * 8) + "px";
+  ball.style.fontSize = (currentBallWeigth * 3) + "px";
+  ball.textContent = currentBallWeigth + "kg";
+  plank.append(ball);
+
+  nextBallWeigth = Math.round(Math.random() * 9) + 1;
+  nextWeigth.textContent = "Next: " + nextBallWeigth + "kg";
+  ballFrame.querySelectorAll('.ball').forEach(ball => ball.remove());
+  createBall(event);
+};
+
+function resetGame() {
+  tiltAngle = 0;
+  leftWeigthValue = 0;
+  rightWeigthValue = 0;
+  nextBallWeigth = Math.round(Math.random() * 9) + 1;
+  const allBalls = plank.querySelectorAll(".ball");
+  allBalls.forEach(ball => ball.remove());
+}
